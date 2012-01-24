de30a61d32659a404cd0c58ac7569f119705bd8b
A. Jesse Jiryu Davis <jesse@10gen.com>
2012-01-24 18:30:01 -0500

    A greenlet-aware pool

--- test/test_gevent.py
+++ test/test_gevent.py
@@ -1,11 +1,30 @@
-import unittest
-import time
+import os
 import threading
+import time
+import unittest
 
 from nose.plugins.skip import SkipTest
 
+from pymongo import pool
 from test_connection import get_connection
 from testutils import delay
+
+host = os.environ.get("DB_IP", "localhost")
+port = int(os.environ.get("DB_PORT", 27017))
+
+def looplet(greenlets):
+    """World's smallest event loop; run until all greenlets are done
+    """
+    while True:
+        done = True
+
+        for g in greenlets:
+            if not g.dead:
+                done = False
+                g.switch()
+
+        if done:
+            return
 
 
 class GeventTest(unittest.TestCase):
@@ -172,7 +191,82 @@ class GeventTest(unittest.TestCase):
             'find_fast start',
             'find_fast done',
             'find_slow done',
-            ], history)
+        ], history)
+
+    def test_greenlet(self):
+        try:
+            import greenlet
+        except ImportError:
+            raise SkipTest('greenlet not installed')
+
+        cx_pool = pool.Pool(
+            pair=(host,port),
+            max_size=10,
+            net_timeout=1000,
+            conn_timeout=1000,
+            use_ssl=False
+        )
+
+        sock_ids = []
+
+        def get_socket():
+            sock_ids.append(id(cx_pool.get_socket()))
+
+        looplet([
+            greenlet.greenlet(get_socket),
+            greenlet.greenlet(get_socket),
+        ])
+
+        self.assertEqual(2, len(sock_ids))
+        self.assertEqual(sock_ids[0], sock_ids[1])
+
+    def test_greenlet_request(self):
+        try:
+            import greenlet
+        except ImportError:
+            raise SkipTest('greenlet not installed')
+
+        # If greenlet module is available, then we can import greenlet_pool
+        from pymongo import greenlet_pool
+
+        pool_args = dict(
+            pair=(host,port),
+            max_size=10,
+            net_timeout=1000,
+            conn_timeout=1000,
+            use_ssl=False,
+        )
+
+        for pool_class, expect_success in [
+            (pool.Pool, False),
+            (greenlet_pool.GreenletPool, True),
+        ]:
+            cx_pool = pool_class(**pool_args)
+            sock_ids = []
+            main = greenlet.getcurrent()
+
+            def get_socket_in_request():
+                cx_pool.start_request()
+                main.switch()
+                sock_ids.append(id(cx_pool.get_socket()))
+                cx_pool.end_request()
+
+            looplet([
+                greenlet.greenlet(get_socket_in_request),
+                greenlet.greenlet(get_socket_in_request),
+            ])
+
+            self.assertEqual(2, len(sock_ids))
+            if expect_success:
+                self.assertNotEqual(
+                    sock_ids[0], sock_ids[1],
+                    "Expected two greenlets to get two different sockets"
+                )
+            else:
+                self.assertEqual(
+                    sock_ids[0], sock_ids[1],
+                    "Expected two greenlets to get same socket"
+                )
 
 if __name__ == '__main__':
     unittest.main()
